9b117297d35b72fa0cdb3465c6646ddb83b10407
pucciquinn@gmail.com <m@ms-MacBook-Air.local>
2026-03-16 08:42:54 -0600

    completed ex 8 and 9 from ch17

--- src/Ch19ex8and9.java
+++ src/Ch19ex8and9.java
@@ -7,19 +7,18 @@ Exercise 9: (1) Modify Exercise 8 to force all the lines in the LinkedList to
 uppercase and send the results to System.out.
  */
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
 public class Ch19ex8and9 {
     public static void main(String args[]) {
-
+        String fileName = args[0]; // exercise 8
+        // unlike scanner, this takes the file name as an argument
+        // when the program is using java Ch19ex8and9 sometext.txt
         BufferedReader in = null;
         // declare input
         try {
-            in = new BufferedReader(new FileReader("sometext.txt"));
+            in = new BufferedReader(new FileReader(fileName));
             // open the input and use it within the try
             LinkedList<String> list = new LinkedList<String>();
             String s;
@@ -27,8 +26,10 @@ public class Ch19ex8and9 {
                 list.addFirst(s);
             // this is how the reverse order happens. placing it at the top of the linked list (First)
             // instead of inputting normally and printing reverse later, builds the list in reverse as it goes
-            for(int i = 0; i < list.size(); i++)
-                System.out.println(list.get(i));
+            for(int i = 0; i < list.size(); i++){
+                System.out.println(list.get(i).toUpperCase()); // print as uppercase (exercise 9)
+            }
+
             // since the list is already in reverse order, just loop and print
         }
         catch(FileNotFoundException e) {
@@ -45,6 +46,4 @@ public class Ch19ex8and9 {
             catch (Exception e) { }
         }
     }
-}
-
 }
